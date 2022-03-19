2f706c6acd1c9a68165b808e5144503b6ca94c7d
Germán Gasset <gassetgerman@gmail.com>
2022-03-19 15:38:33 +0100

    updated NN.getgrads so it asks for executionresults instead of generating them, added GetSupervisedGrads

--- NN/Network/NN.cs
+++ NN/Network/NN.cs
@@ -12,9 +12,9 @@ namespace NN
         public int Length => layers.Count;
         public int inputLength => layers.Count > 0 ? layers[0].PrevLayerLength : 0;
         internal List<Layer> layers;
-        Activation.ActivationFunctions activationFunction;
+        public readonly Activation.ActivationFunctions activationFunction;
 
-        /// <param name="topology">neuronCount at each layer, this includes output</param>
+        /// <param name="topology">neuronCount at each layer, this includes output layer</param>
         public NN(int inputLength, int[] topology, Activation.ActivationFunctions activationFunc)
         {
             layers = new List<Layer>();
@@ -42,6 +42,9 @@ namespace NN
         /// <returns>All executions results</returns>
         public List<double[]> ExecuteNetwork(double[] input, out double[] output)
         {
+            if (input.Length != inputLength)
+                throw new IndexOutOfRangeException();
+
             List<double[]> outputs = new List<double[]>();
             double[] prevLayerOuts = input;
 
@@ -57,10 +60,22 @@ namespace NN
             return outputs;
         }
 
-        public List<LayerVs> GetGrads(double[] input, double[] costs)
+        public List<LayerVs> GetSupervisedGrads(double[] input, double[] expected, CostFunctions costFunction)
+        {
+            List<double[]> neuronActivations = ExecuteNetwork(input, out double[] output);
+            if (expected.Length != output.Length)
+                throw new IndexOutOfRangeException("Expected Output Count is Incorrect");
+
+            double[] costs = new double[output.Length];
+            for (int i = 0; i < output.Length; i++)
+                costs[i] = Derivatives.DerivativeOf(output[i], expected[i], costFunction);
+
+            return GetGrads(input, neuronActivations, costs);
+        }
+
+        public List<LayerVs> GetGrads(double[] input, List<double[]> layerOuts, double[] costs)
         {
             List<LayerVs> grads = new List<LayerVs>();
-            List<double[]> layerOuts = ExecuteNetwork(input, out _);
             for (int layerI = Length - 1; layerI >= 0; layerI--)
             {
                 layers[layerI].GetGrads(layerI > 0 ? layerOuts[layerI - 1] : input, costs, activationFunction, out costs, out LayerVs layerGrads);
